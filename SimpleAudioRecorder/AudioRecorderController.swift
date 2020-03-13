e8d8880c3c73e140fd9f4fed77a8842688987412
Dillon <dillonp.lambda@gmail.com>
2020-03-12 19:20:45 -0700

    Fixed issue where pause button would not revert to play button when audio finished playing.

--- SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/AudioRecorderController.swift
@@ -11,7 +11,12 @@ import AVFoundation
 
 class AudioRecorderController: UIViewController {
     
-    var audioPlayer: AVAudioPlayer?
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            guard let audioPlayer = audioPlayer else { return }
+            audioPlayer.delegate = self
+        }
+    }
     
     @IBOutlet var playButton: UIButton!
     @IBOutlet var recordButton: UIButton!
@@ -203,3 +208,16 @@ class AudioRecorderController: UIViewController {
     }
 }
 
+extension AudioRecorderController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Audio player error: \(error)")
+        }
+    }
+    
+    
+}
